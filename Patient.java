06f8d3ec08452bc1fd1e040ae814aae82473eac8
DonOndeje <donondeje@gmail.com>
2017-04-27 10:46:46 +0300

    Add files via upload

--- Patient.java
+++ Patient.java
@@ -1,6 +1,7 @@
 
 package farlesmedical;
 
+import java.sql.Date;
 import javafx.beans.property.StringProperty; //defines read-only properties and writable properties, plus a number of implementations.
 import javafx.beans.property.SimpleStringProperty;
 
@@ -9,12 +10,36 @@ import javafx.beans.property.SimpleStringProperty;
 public class Patient {
     // Declare VAriables to hold the field vales from our patient table.
      private final  StringProperty PatientNo = new SimpleStringProperty(this,"PatientNo");
+     private final StringProperty Date = new SimpleStringProperty(this,"Date");
      private final StringProperty firstName = new SimpleStringProperty(this,"firstName");
     private final StringProperty lastName = new SimpleStringProperty(this, "lastName");
+    private final StringProperty Sex = new SimpleStringProperty(this, "Sex");
     private final StringProperty Inference = new SimpleStringProperty(this,"Inference");
     private final StringProperty Residence = new SimpleStringProperty(this, "Residence");
     private final StringProperty PhoneNumber = new SimpleStringProperty(this, "PhoneNumber");
    
+   
+    
+    
+      public StringProperty SexProperty(){
+      return Sex;
+  }
+      public String getsexProperty(){
+      return SexProperty().get();
+  }
+     public final void setSex(String Sex){
+        SexProperty().set(Sex);
+    }
+    
+     public StringProperty DateProperty(){
+      return Date;
+  }
+    public StringProperty getDateProperty(){
+        return DateProperty();
+    }
+          public final void setDate(String date){
+         getDateProperty().set(date);
+    }
     
   public StringProperty PatientNoProperty(){
       return PatientNo;
@@ -80,12 +105,14 @@ public StringProperty InferenceProperty(){
      InferenceProperty().set(Inference);
   }
 
-    public Patient(String firstName, String lastName, String PhoneNumber,String Inference,String Residence) {
+    public Patient(String firstName, String lastName, String PhoneNumber,String Sex,String Inference,String Residence,String date) {
         setFirstName(firstName);
         setLastName(lastName);
         setPhoneNumber(PhoneNumber);
+        setSex(Sex);
         setInference(Inference);
         setResidence(Residence);
+        setDate(date);
     }
     
  
